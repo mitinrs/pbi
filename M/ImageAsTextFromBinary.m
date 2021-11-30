4b42e60ce2a1395ba4421592c1d5c73cd019e903
mitinrs <mitinrs@gmail.com>
2021-11-30 13:23:52 +0300

    upd

--- M/ImageAsTextFromBinary.m
+++ M/ImageAsTextFromBinary.m
@@ -1,9 +1,9 @@
 let
     BinaryToImage = (ImageBinary as binary) as list =>
-        let
-            Base64 = "data:image/jpeg;base64, " & Binary.ToText(ImageBinary, BinaryEncoding.Base64),
-            SplitUpText=Splitter.SplitTextByRepeatedLengths(30000)(Base64)
-        in
-            SplitUpText
+    let
+        Base64 = "data:image/jpeg;base64, " & Binary.ToText(ImageBinary, BinaryEncoding.Base64),
+        SplitUpText=Splitter.SplitTextByRepeatedLengths(30000)(Base64)
+    in
+        SplitUpText
 in
     BinaryToImage
--- M/ImageAsTextFromLink.m
+++ M/ImageAsTextFromLink.m
@@ -1,10 +1,10 @@
 let
     UrlToImage = (ImageUrl as text) as list =>
-let
-    BinaryContent = Web.Contents(ImageUrl),
-    Base64 = "data:image/jpeg;base64, " & Binary.ToText(BinaryContent, BinaryEncoding.Base64),
-    SplitUpText=Splitter.SplitTextByRepeatedLengths(30000)(Base64)
-in
-    SplitUpText
+    let
+        BinaryContent = Web.Contents(ImageUrl),
+        Base64 = "data:image/jpeg;base64, " & Binary.ToText(BinaryContent, BinaryEncoding.Base64),
+        SplitUpText=Splitter.SplitTextByRepeatedLengths(30000)(Base64)
+    in
+        SplitUpText
 in
     UrlToImage
